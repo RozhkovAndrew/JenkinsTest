7d85a47ebd80f44d83fe50bf9a2edb517805287f
Andrey Rozhkov <andrew93pav@gmail.com>
2026-07-22 22:32:22 +0500

    Jenkins: Автоматическое удаление файлов [13]

---
+++ project@tmp/durable-a4c6d32a/script.sh
@@ -0,0 +1,6 @@
+
+                            git add -A
+                            git config user.name "Andrey Rozhkov"
+                            git config user.email "andrew93pav@gmail.com"
+                            git commit -m "Jenkins: Автоматическое удаление файлов [13]" || echo "Нет изменений для коммита"
+                        
